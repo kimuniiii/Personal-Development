6c831562438b2745ab548009f22ffca07c6b89a8
k-kimura <kk.baseball.0824@gmail.com>
2021-07-11 01:29:10 +0900

    共通のテンプレートを作成

---
+++ packages/client/src/components/templates/CommonTemplate/index.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode, VFC } from "react";
+
+import { CommonFooter } from "src/components/templates/CommonTemplate/CommonFooter";
+import { CommonHeader } from "src/components/templates/CommonTemplate/CommonHeader";
+
+type CommonTemplateProps = {
+  children: ReactNode;
+};
+
+export const CommonTemplate: VFC<CommonTemplateProps> = ({ children }) => {
+  return (
+    <div>
+      <CommonHeader />
+      <main>{children}</main>
+      <CommonFooter />
+    </div>
+  );
+};
--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,5 +1,15 @@
+import React from "react";
+
+import { HeadTemplate } from "../components/templates/HeadTemplate";
+import { CommonTemplate } from "src/components/templates/CommonTemplate";
+
 const TopPage = () => {
-  return <div>TopPage</div>;
+  return (
+    <React.Fragment>
+      <HeadTemplate pageTitle="TopPage" />
+      <CommonTemplate>TopPage</CommonTemplate>
+    </React.Fragment>
+  );
 };
 
 export default TopPage;
