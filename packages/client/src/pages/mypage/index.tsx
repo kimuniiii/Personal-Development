0b30484b7e82ae82d56590a84d50b14ed88713d3
k-kimura <kk.baseball.0824@gmail.com>
2021-07-11 02:26:05 +0900

    マイページを簡易的に作成

---
+++ packages/client/src/pages/mypage/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { HeadTemplate } from "src/components/templates/HeadTemplate";
+import { CommonTemplate } from "src/components/templates/CommonTemplate";
+
+const MyPage = () => {
+  return (
+    <React.Fragment>
+      <HeadTemplate pageTitle="マイページ" />
+      <CommonTemplate>マイページ</CommonTemplate>
+    </React.Fragment>
+  );
+};
+
+export default MyPage;
