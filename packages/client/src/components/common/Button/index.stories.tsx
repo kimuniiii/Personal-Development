9c20ee1802e7c88619b4edb9d9682b75a42cf1d2
k-kimura <kk.baseball.0824@gmail.com>
2021-07-11 03:30:53 +0900

    ButtonコンポーネントのStorybook作成

---
+++ packages/client/src/components/common/Button/index.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+
+import { Button } from ".";
+
+export default {
+  title: "common/Button",
+  component: Button,
+} as Meta;
+
+type Props = React.ComponentProps<typeof Button>;
+
+const Template: Story<Props> = (args) => <Button {...args} />;
+
+export const Basic = Template.bind({});
+
+Basic.args = {
+  buttonContent: "Button",
+};
