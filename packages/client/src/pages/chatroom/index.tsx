3f3fe168b648b3c48bf3c641c3bacf4b66752194
k-kimura <kk.baseball.0824@gmail.com>
2021-07-11 02:52:50 +0900

    連絡掲示版画面を簡易的に作成

---
+++ packages/client/src/pages/chatroom/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { HeadTemplate } from "src/components/templates/HeadTemplate";
+import { CommonTemplate } from "src/components/templates/CommonTemplate";
+
+const Chatroom = () => {
+  return (
+    <React.Fragment>
+      <HeadTemplate pageTitle="連絡掲示板" />
+      <CommonTemplate>連絡掲示板</CommonTemplate>
+    </React.Fragment>
+  );
+};
+
+export default Chatroom;
