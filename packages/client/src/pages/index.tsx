75b0c569e1ae6ae0c73b92b6ff909c21ff096df3
k-kimura <kk.baseball.0824@gmail.com>
2021-07-14 11:55:11 +0900

    React.Fragment を <> に変更。エラー出ないことを確認。

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-
 import { HeadTemplate } from "src/components/templates/HeadTemplate";
 import { CommonTemplate } from "src/components/templates/CommonTemplate";
 
 const TopPage = () => {
   return (
-    <React.Fragment>
+    <>
       <HeadTemplate pageTitle="トップページ" />
       <CommonTemplate>TopPage</CommonTemplate>
-    </React.Fragment>
+    </>
   );
 };
 
--- packages/client/src/pages/product/detail.tsx
+++ packages/client/src/pages/product/detail.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-
 import { HeadTemplate } from "src/components/templates/HeadTemplate";
 import { CommonTemplate } from "src/components/templates/CommonTemplate";
 
 const ProductDetailPage = () => {
   return (
-    <React.Fragment>
+    <>
       <HeadTemplate pageTitle="商品詳細ページ" />
       <CommonTemplate>商品詳細ページ</CommonTemplate>
-    </React.Fragment>
+    </>
   );
 };
 
