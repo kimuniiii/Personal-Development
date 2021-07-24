ebae6423bd2d7446951bdd5993598b47c9f2a012
k-kimura <kk.baseball.0824@gmail.com>
2021-07-24 19:54:31 +0900

    extendsにReact用のプラグインを追加する

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -5,5 +5,11 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
 };
