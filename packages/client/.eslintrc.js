269b38e9ec7bdbb6c935acfc96ff6cc052705dd7
k-kimura <kk.baseball.0824@gmail.com>
2021-07-24 19:52:05 +0900

    envプロパティを使うことで静的検証の前提条件を設定してエラー解消

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -1,6 +1,8 @@
-// eslint-disable-next-line no-undef
 module.exports = {
   root: true,
+  env: {
+    node: true,
+  },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
