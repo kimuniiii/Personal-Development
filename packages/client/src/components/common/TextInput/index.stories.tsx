a7089ad437c71e60013a494e3f6d3a2dad72395f
k-kimura <kk.baseball.0824@gmail.com>
2021-08-07 00:50:53 +0900

    Inputコンポーネントを実装完了

---
+++ packages/client/src/components/common/TextInput/index.stories.tsx
@@ -0,0 +1,56 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import { Input } from '.';
+
+export default {
+  component: Input,
+  title: 'common/Input',
+} as Meta;
+
+type Props = React.ComponentProps<typeof Input>;
+
+/**
+ * @Memo mode: 'onChange'にするとエラーになるので、バリデーションの挙動はStorybookで確認できない
+ * @Error RangeError: Maximum call stack size exceeded
+ **/
+const Template: Story<Props> = (args) => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm({
+    mode: 'onSubmit',
+    reValidateMode: 'onSubmit',
+  });
+
+  return (
+    <>
+      <Input
+        {...args}
+        isError={!!errors.firstName}
+        errors={errors}
+        register={register('firstName', {
+          pattern: {
+            message: '大文字のアルファベットで入力してください！',
+            value: /^[A-Z]+$/,
+          },
+          required: { message: '必須入力項目です！', value: true },
+        })}
+      />
+    </>
+  );
+};
+
+export const Basic = Template.bind({});
+
+Basic.args = {
+  disabled: false,
+  type: 'email',
+  fontSizeValue: '12px',
+  id: 'email',
+  labelText: 'Email',
+  name: 'email',
+  placeholder: '例: riot-ec-site@gmail.com',
+  width: '343px',
+};
---
+++ packages/client/src/components/common/TextInput/index.tsx
@@ -0,0 +1,149 @@
+import { css, SerializedStyles } from '@emotion/react';
+import styled from '@emotion/styled';
+import { ErrorMessage } from '@hookform/error-message';
+import React from 'react';
+
+import type { UseFormRegisterReturn } from 'react-hook-form';
+import type { ValueOf } from 'typings/ValueOf';
+
+import { Margin } from 'src/components/layouts/Margin';
+
+import { COLOR_PALETTE } from 'src/styles/color_palette';
+import { FONT_SIZE } from 'src/styles/font_size';
+
+type InputProps = JSX.IntrinsicElements['input'] & {
+  type: string;
+  name: string;
+  width: string;
+  placeholder: string;
+  isError: boolean;
+  errors: Record<string, unknown>;
+  register: UseFormRegisterReturn;
+  id?: string;
+  labelText?: string;
+  disabled?: boolean;
+  fontSizeValue?: ValueOf<typeof FONT_SIZE>;
+};
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      type,
+      name,
+      width,
+      placeholder,
+      isError,
+      errors,
+      register,
+      id = 'text-input',
+      labelText = '',
+      disabled = false,
+      fontSizeValue,
+      ...textInputProps
+    },
+    ref,
+  ) => {
+    return (
+      <StTextField>
+        {labelText !== '' ? (
+          <StLabel htmlFor={id} fontSizeValue={fontSizeValue}>
+            {labelText}
+          </StLabel>
+        ) : null}
+        <StInput
+          {...textInputProps}
+          type={type}
+          id={id}
+          width={width}
+          disabled={disabled}
+          fontSizeValue={fontSizeValue}
+          placeholder={placeholder}
+          isError={isError}
+          {...register}
+          ref={ref}
+        />
+        {isError ? (
+          <ErrorMessage
+            errors={errors}
+            name={name}
+            render={({ message }): JSX.Element => (
+              <React.Fragment>
+                <Margin bottom='8px' />
+                <StErrorMessage>{message}</StErrorMessage>
+              </React.Fragment>
+            )}
+          />
+        ) : null}
+      </StTextField>
+    );
+  },
+);
+
+Input.displayName = 'TextInput';
+
+const StTextField = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StLabel = styled.label<Pick<InputProps, 'fontSizeValue'>>`
+  padding-bottom: 8px;
+  font-size: ${({ fontSizeValue }): ValueOf<typeof FONT_SIZE> => fontSizeValue ?? FONT_SIZE.FS_16};
+  cursor: pointer;
+`;
+
+type StInputProps = Pick<InputProps, 'width' | 'fontSizeValue' | 'disabled' | 'isError'>;
+
+const StInput = styled.input<StInputProps>`
+  display: block;
+  width: ${({ width }): string => width};
+  height: 52px;
+  padding: 0 16px;
+  border: solid 1px ${COLOR_PALETTE.DARK_GRAY};
+  border-radius: 3px;
+  outline: 0;
+  font-size: ${({ fontSizeValue }): ValueOf<typeof FONT_SIZE> => fontSizeValue ?? FONT_SIZE.FS_16};
+  color: ${COLOR_PALETTE.BLACK};
+  cursor: pointer;
+
+  &:hover {
+    border: 1px solid ${COLOR_PALETTE.MAIN_COLOR};
+  }
+
+  &:focus {
+    border: 2px solid ${COLOR_PALETTE.MAIN_COLOR};
+  }
+
+  &::placeholder {
+    font-size: ${({ fontSizeValue }): ValueOf<typeof FONT_SIZE> =>
+      fontSizeValue ?? FONT_SIZE.FS_16};
+    color: ${COLOR_PALETTE.DARK_GRAY};
+  }
+
+  ${({ isError }): SerializedStyles | null =>
+    isError
+      ? css`
+          border: solid 2px ${COLOR_PALETTE.ERROR_COLOR};
+          background-color: ${COLOR_PALETTE.WARNING_COLOR};
+
+          &:hover,
+          &:focus {
+            border: solid 2px ${COLOR_PALETTE.ERROR_COLOR};
+            background-color: ${COLOR_PALETTE.WARNING_COLOR};
+          }
+        `
+      : null}
+
+  ${({ disabled }): SerializedStyles | null =>
+    disabled
+      ? css`
+          background-color: ${COLOR_PALETTE.GRAY};
+          cursor: not-allowed;
+          opacity: 0.3;
+        `
+      : null}
+`;
+
+const StErrorMessage = styled.div`
+  color: ${COLOR_PALETTE.ERROR_COLOR};
+`;
