ec66befb8542cad1aea15ef2d41c21ef891628e6
k-kimura <kk.baseball.0824@gmail.com>
2021-08-06 14:21:48 +0900

    Headerをデザイン通りに簡易的に実装完了

--- packages/client/src/components/templates/CommonTemplate/CommonHeader/index.tsx
+++ packages/client/src/components/templates/CommonTemplate/CommonHeader/index.tsx
@@ -1,5 +1,47 @@
+import styled from '@emotion/styled';
+
 import type { VFC } from 'react';
 
+import { Button } from 'src/components/common/Button';
+
+import { COLOR_PALETTE } from 'src/styles/color_palette';
+
 export const CommonHeader: VFC = () => {
-  return <header>Header</header>;
+  return (
+    <StHeader>
+      <strong>Riot</strong>
+      <StButtonContainer>
+        <Button
+          type='button'
+          styleTypes='textLink'
+          width='100px'
+          fontSizeValue='16px'
+          padding='0'
+          buttonContent='ログイン'
+          onClick={(): void => alert('clicked Login Button')}
+        />
+        <Button
+          type='button'
+          styleTypes='textLink'
+          width='100px'
+          fontSizeValue='16px'
+          padding='0'
+          buttonContent='ユーザー登録'
+          onClick={(): void => alert('clicked User Register Button')}
+        />
+      </StButtonContainer>
+    </StHeader>
+  );
 };
+
+const StHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+  background-color: ${COLOR_PALETTE.LIGHT_GRAY};
+`;
+
+const StButtonContainer = styled.div`
+  display: flex;
+`;
