43a2e4c4606253d91929f04afd133279f92c2789
k-kimura <kk.baseball.0824@gmail.com>
2021-08-06 13:16:07 +0900

    全てのページコンポーネントでグローバルスタイルが反映されるようにAppを設定

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,7 +1,28 @@
-import { AppProps } from 'next/app';
+import { css, Global } from '@emotion/react';
+import App, { AppContext, AppInitialProps, AppProps } from 'next/app';
+import React from 'react';
+
+import { HeadTemplate } from 'src/components/templates/HeadTemplate';
+
+import { GLOBAL_STYLE } from 'src/styles/global_style';
 
 const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <HeadTemplate />
+      <Global
+        styles={css`
+          ${GLOBAL_STYLE}
+        `}
+      />
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 };
 
 export default CustomApp;
+
+CustomApp.getInitialProps = async (appContext: AppContext): Promise<AppInitialProps> => {
+  const appProps = await App.getInitialProps(appContext);
+  return { ...appProps };
+};
