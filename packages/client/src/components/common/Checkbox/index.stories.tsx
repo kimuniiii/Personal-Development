c94686c51157ff7dc8e03e4148495d670ac0d506
k-kimura <kk.baseball.0824@gmail.com>
2021-08-07 01:51:03 +0900

    ログイン画面で使用するCheckboxコンポーネントを新規実装

---
+++ packages/client/src/components/common/Checkbox/index.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React, { useState } from 'react';
+
+import { Checkbox } from '.';
+
+export default {
+  component: Checkbox,
+  title: 'common/Checkbox',
+} as Meta;
+
+type Props = React.ComponentProps<typeof Checkbox>;
+
+const Template: Story<Props> = (args) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = (): void => {
+    setIsChecked((prev: boolean) => !prev);
+  };
+
+  return <Checkbox {...args} checked={isChecked} onChange={handleChange} />;
+};
+
+export const Basic = Template.bind({});
+
+Basic.args = {
+  disabled: false,
+  labelText: '次回ログインを省略する',
+};
---
+++ packages/client/src/components/common/Checkbox/index.tsx
@@ -0,0 +1,67 @@
+import { css, SerializedStyles } from '@emotion/react';
+import styled from '@emotion/styled';
+import React from 'react';
+
+import type { UseFormRegisterReturn } from 'react-hook-form';
+import type { ValueOf } from 'typings/ValueOf';
+
+import { COLOR_PALETTE } from 'src/styles/color_palette';
+import { FONT_SIZE } from 'src/styles/font_size';
+import { FONT_WEIGHT } from 'src/styles/font_weight';
+
+type CheckboxProps = JSX.IntrinsicElements['input'] & {
+  checked: boolean;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  register: UseFormRegisterReturn;
+  labelText?: string;
+};
+
+export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ checked, onChange, register, labelText = '', ...inputProps }, ref) => {
+    // undefined を取り除くため
+    const isDisabled = !!inputProps['disabled'];
+
+    return (
+      <StLabel disabled={isDisabled}>
+        <StInput
+          {...inputProps}
+          type='checkbox'
+          checked={checked}
+          {...register}
+          onChange={onChange}
+          ref={ref}
+        />
+        {labelText !== '' ? <StLabelText disabled={isDisabled}>{labelText}</StLabelText> : null}
+      </StLabel>
+    );
+  },
+);
+
+Checkbox.displayName = 'Checkbox';
+
+const StLabel = styled.label<Pick<CheckboxProps, 'disabled'>>`
+  display: inline-flex;
+  align-items: center;
+  cursor: pointer;
+  user-select: none;
+
+  ${({ disabled }): SerializedStyles | null =>
+    disabled
+      ? css`
+          cursor: not-allowed;
+          opacity: 0.3;
+        `
+      : null}
+`;
+
+const StInput = styled.input`
+  cursor: pointer;
+`;
+
+const StLabelText = styled.span<Pick<CheckboxProps, 'disabled'>>`
+  padding-left: 8px;
+  font-size: ${FONT_SIZE.FS_16};
+  font-weight: ${FONT_WEIGHT.BOLD};
+  color: ${({ disabled }): ValueOf<typeof COLOR_PALETTE> =>
+    disabled ? COLOR_PALETTE.BLACK : COLOR_PALETTE.SUB_COLOR};
+`;
