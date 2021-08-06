5caf60f9c80b73af9a0d30be9ee995a09dc9bc14
k-kimura <kk.baseball.0824@gmail.com>
2021-08-06 13:23:17 +0900

    カスタムDocumentコンポーネントを定義する

---
+++ packages/client/src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+// eslint-disable-next-line
+// @ts-ignore
+import React from 'react';
+
+export default class CustomDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
