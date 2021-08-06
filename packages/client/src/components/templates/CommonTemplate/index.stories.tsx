9221eefd5da14b047a64ea4711f98af10873c6c6
k-kimura <kk.baseball.0824@gmail.com>
2021-08-06 15:17:53 +0900

    CommonTemplateコンポーネントのストーリーファイルを作成

---
+++ packages/client/src/components/templates/CommonTemplate/index.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { CommonTemplate } from '.';
+
+export default {
+  title: 'templates/CommonTemplate',
+  component: CommonTemplate,
+} as Meta;
+
+type Props = React.ComponentProps<typeof CommonTemplate>;
+
+const Template: Story<Props> = (args) => <CommonTemplate {...args} />;
+
+export const Basic = Template.bind({});
+
+Basic.args = {
+  children: 'CommonTemplate',
+};
