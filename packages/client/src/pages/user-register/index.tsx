01ab02c31e74242beb8a4fea40490a38a25ccca9
k-kimura <kk.baseball.0824@gmail.com>
2021-08-07 01:27:49 +0900

    ユーザー登録画面をデザイン通りに実装

--- packages/client/src/pages/user-register/index.tsx
+++ packages/client/src/pages/user-register/index.tsx
@@ -1,16 +1,126 @@
-import { HeadTemplate } from 'src/components/templates/HeadTemplate';
+import styled from '@emotion/styled';
+import { useForm } from 'react-hook-form';
+
+import { Button } from 'src/components/common/Button';
 import { CommonTemplate } from 'src/components/templates/CommonTemplate';
+import { HeadTemplate } from 'src/components/templates/HeadTemplate';
+import { Input } from 'src/components/common/Input';
+import { Margin } from 'src/components/layouts/Margin';
+
+import { COLOR_PALETTE } from 'src/styles/color_palette';
 
 /**
  * @概要 ユーザー登録ボタンを押したら表示されるページコンポーネント
  */
 const UserRegisterPage = (): JSX.Element => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: 'onChange',
+    reValidateMode: 'onChange',
+  });
+
+  /**
+   * 送信ボタンを押した時に呼び出されるイベントハンドラ
+   */
+  const onSubmit = (data: unknown): void => {
+    console.log(data);
+  };
+
   return (
     <>
       <HeadTemplate pageTitle='ユーザー登録ページ' />
-      <CommonTemplate>User Register Page</CommonTemplate>
+      <CommonTemplate>
+        <StUserRegisterFormContainer onSubmit={handleSubmit(onSubmit)}>
+          <h3>ユーザ登録</h3>
+          <StWithDrawContainer>
+            <Input
+              type='email'
+              id='email'
+              name='email'
+              labelText='Email'
+              placeholder='メールアドレスを入力してください'
+              width='343px'
+              fontSizeValue='12px'
+              isError={!!errors.email}
+              errors={errors}
+              register={register('email', {
+                pattern: {
+                  message: 'メールアドレスの書き方が間違ってます！',
+                  value: /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
+                },
+                required: { message: '必須入力項目です！', value: true },
+              })}
+            />
+            <Input
+              type='password'
+              id='password'
+              name='password'
+              labelText='パスワード'
+              placeholder='パスワードを入力してください'
+              width='343px'
+              fontSizeValue='12px'
+              isError={!!errors.password}
+              errors={errors}
+              register={register('password', {
+                required: { message: '必須入力項目です！', value: true },
+              })}
+            />
+            <Input
+              type='password'
+              id='re-password'
+              name='re-password'
+              labelText='パスワード(再入力)'
+              placeholder='再度、パスワードを入力してください'
+              width='343px'
+              fontSizeValue='12px'
+              isError={!!errors.password}
+              errors={errors}
+              register={register('re-password', {
+                required: { message: '必須入力項目です！', value: true },
+              })}
+            />
+            <Margin bottom='16px' />
+            <Button
+              type='submit'
+              styleTypes='tertiary'
+              width='200px'
+              fontSizeValue='16px'
+              buttonContent='登録する'
+              disabled={!isValid}
+              onClick={(): void => alert('登録するボタンをクリック')}
+            />
+          </StWithDrawContainer>
+        </StUserRegisterFormContainer>
+      </CommonTemplate>
     </>
   );
 };
 
 export default UserRegisterPage;
+
+const StUserRegisterFormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /*
+  ** CommonHeaderのheight : 48px
+  ** CommonFooterのheight : 56px
+  */
+  min-height: calc(100vh - (48px + 56px));
+  padding: 16px;
+`;
+
+const StWithDrawContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  width: 375px;
+  height: auto;
+  padding: 16px;
+  background-color: ${COLOR_PALETTE.LIGHT_GRAY};
+`;
