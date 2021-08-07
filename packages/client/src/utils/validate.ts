7ac55248fc2ed6f917c404de1028af927859093b
k-kimura <kk.baseball.0824@gmail.com>
2021-08-07 16:46:19 +0900

    email・telephone・postcode・firstName・lastName

--- packages/client/src/utils/validate.ts
+++ packages/client/src/utils/validate.ts
@@ -1,3 +1,7 @@
 export const validations = {
   email: /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
-};
+  firstName: /^[ァ-ヴ]+$/,
+  lastName: /^[ァ-ヴ]+$/,
+  telephone: /^0(\d-\d{4}|\d{2}-\d{3}|\d{3}-\d{2}|\d{4}-\d)-\d{4}$/,
+  postcode: /^\d{3}\d{4}$/,
+} as const;
