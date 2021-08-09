d11a89f6619c810c2ede46d3d97b1b471f3b0920
k-kimura <kk.baseball.0824@gmail.com>
2021-08-09 13:39:09 +0900

    ログイン → /sign-in・ユーザー登録 → /sign-up

--- packages/client/src/components/templates/CommonTemplate/CommonHeader/index.tsx
+++ packages/client/src/components/templates/CommonTemplate/CommonHeader/index.tsx
@@ -28,7 +28,7 @@ export const CommonHeader: VFC = () => {
           fontSizeValue='16px'
           padding='0'
           buttonContent='ログイン'
-          onClick={(): Promise<boolean> => Router.push('/login')}
+          onClick={(): Promise<boolean> => Router.push('/sign-in')}
         />
         <Button
           type='button'
@@ -37,7 +37,7 @@ export const CommonHeader: VFC = () => {
           fontSizeValue='16px'
           padding='0'
           buttonContent='ユーザー登録'
-          onClick={(): Promise<boolean> => Router.push('/user-register')}
+          onClick={(): Promise<boolean> => Router.push('/sign-up')}
         />
       </StButtonContainer>
     </StHeader>
