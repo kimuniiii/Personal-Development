d99155c229af29e8f4b149dac570a6fb89fa8b2b
k-kimura <kk.baseball.0824@gmail.com>
2021-08-18 01:15:03 +0900

    写真アップロードコンポーネントのデザイン完成

--- packages/client/src/components/organisms/PhotoUpload/index.tsx
+++ packages/client/src/components/organisms/PhotoUpload/index.tsx
@@ -1,14 +1,91 @@
-import styled from '@emotion/styled';
-// eslint-disable-next-line
-// @ts-ignore
+import { ImCross } from 'react-icons/im';
+import Image from 'next/image';
 import React, { VFC } from 'react';
+import styled from '@emotion/styled';
 
-type Props = {
-  text: string;
+import { IconButton } from 'src/components/atoms/IconButton';
+import { Margin } from 'src/components/layouts/Margin';
+
+import { COLOR_PALETTE } from 'src/styles/color_palette';
+import { FONT_SIZE } from 'src/styles/font_size';
+
+import ReactImage from '../../../../public/react.jpg';
+
+export const PhotoUpload: VFC = () => {
+  return (
+    <React.Fragment>
+      <StImageContainer>
+        {[...Array(3)].map((_: number, idx: number) => {
+          return (
+            <StImagePosition key={idx}>
+              <IconButton
+                className='icon-close-button'
+                type='button'
+                svgComponent={<ImCross size={16} />}
+                width='40px'
+                height='40px'
+                borderRadius='50%'
+                onClick={(): void => alert('IconButton Clicked')}
+              />
+              <Image src={ReactImage} alt='Reactの画像です' />
+            </StImagePosition>
+          );
+        })}
+      </StImageContainer>
+      <Margin bottom='16px' />
+      <StFileUploadBtnContainer>
+        <StLabel>
+          写真を追加
+          <input type='file' />
+        </StLabel>
+      </StFileUploadBtnContainer>
+    </React.Fragment>
+  );
 };
 
-export const PhotoUpload: VFC<Props> = ({ text }) => {
-  return <StBase>{text}</StBase>;
-};
+const StImageContainer = styled.section`
+  display: flex;
+  gap: 16px;
+  max-width: 500px;
+`;
 
-const StBase = styled.div``;
+const StImagePosition = styled.div`
+  position: relative;
+  width: 157px;
+  height: 157px;
+
+  .icon-close-button {
+    position: absolute;
+    top: -16px;
+    left: 128px;
+    /* 画像よりもボタンを上に表示させるため */
+    z-index: 1;
+  }
+`;
+
+const StFileUploadBtnContainer = styled.section`
+  display: flex;
+  flex-direction: row-reverse;
+  max-width: 500px;
+`;
+
+const StLabel = styled.label`
+  display: inline-block;
+  text-align: center;
+  padding: 12px 16px;
+  width: 157px;
+  border-radius: 3px; /* 角の丸み */
+  background-color: #3e8bff; /* 背景色 */
+  color: ${COLOR_PALETTE.WHITE}; /* 文字色 */
+  font-size: ${FONT_SIZE.FS_16}; /* フォントサイズ */
+  transition: opacity 0.3s; /* ホバーをなめらかに */
+
+  input[type='file'] {
+    display: none;
+  }
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.6;
+  }
+`;
