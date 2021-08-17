5d2e54c4550ed0a65e9916653250ffc657e0cd62
k-kimura <kk.baseball.0824@gmail.com>
2021-08-18 00:07:59 +0900

    画像アップロードコンポーネントを簡易的に実装

---
+++ packages/client/src/components/organisms/PhotoUpload/index.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+// eslint-disable-next-line
+// @ts-ignore
+import React, { VFC } from 'react';
+
+type Props = {
+  text: string;
+};
+
+export const PhotoUpload: VFC<Props> = ({ text }) => {
+  return <StBase>{text}</StBase>;
+};
+
+const StBase = styled.div``;
