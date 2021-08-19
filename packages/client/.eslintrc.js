84d4c29b49194a54952fe2c082a3eee4d87859bc
k-kimura <kk.baseball.0824@gmail.com>
2021-08-19 10:35:51 +0900

    eslint-plugin-import の設定ファイルの記述完了

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -15,6 +15,15 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'react'],
   settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+        config: 'tsconfig.json',
+      },
+    },
     react: {
       version: 'detect', // React version. "detect" automatically picks the version you have installed.
     },
@@ -32,5 +41,46 @@ module.exports = {
       },
     ],
     '@typescript-eslint/explicit-function-return-type': 'error',
+    'import/order': [
+      'error',
+      {
+        alphabetize: { caseInsensitive: true, order: 'asc' },
+        groups: ['builtin', 'external', 'type', 'index', 'sibling', 'parent'],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            group: 'index',
+            pattern: 'src/components/**',
+            position: 'before',
+          },
+          {
+            group: 'index',
+            pattern: 'src/constants',
+            position: 'before',
+          },
+          {
+            group: 'index',
+            pattern: 'src/hooks/**',
+            position: 'before',
+          },
+          {
+            group: 'index',
+            pattern: 'src/stores/**',
+            position: 'before',
+          },
+          {
+            group: 'index',
+            pattern: 'src/styles/**',
+            position: 'before',
+          },
+          {
+            group: 'index',
+            pattern: 'src/utils/**',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['src/components/**', 'src/styles/**'],
+      },
+    ],
   },
 };
