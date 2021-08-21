098cf5b0f2a97b93773bb2376bd410bf2ce25c70
k-kimura <kk.baseball.0824@gmail.com>
2021-08-21 11:29:51 +0900

    バリデーションのコメントアウトを変更

--- packages/client/src/utils/validate.ts
+++ packages/client/src/utils/validate.ts
@@ -4,7 +4,7 @@ export const validations = {
   lastName: /^[ァ-ヴ]+$/,
   telephone: /^0(\d-\d{4}|\d{2}-\d{3}|\d{3}-\d{2}|\d{4}-\d)-\d{4}$/,
   postcode: /^\d{3}\d{4}$/,
-  ageNumber: /^([1-9]?[0-9]|100)$/, // 1から100までの数字に一致させる
+  ageNumber: /^([1-9]?[0-9]|100)$/, // 0から100までの数字に一致させる
   minPhoneNumberLength: 10,
   maxPhoneNumberLength: 20,
   minPasswordLength: 6,
