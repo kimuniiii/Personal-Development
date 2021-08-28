7a1c63113c27b51c19ea0cc079d2943b73775b11
k-kimura <kk.baseball.0824@gmail.com>
2021-08-28 16:41:19 +0900

    Custom App コンポーネントに「ApolloProvider」を追加する

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { css, Global } from '@emotion/react';
 import App, { AppContext, AppInitialProps, AppProps } from 'next/app';
 import React from 'react';
@@ -6,16 +7,23 @@ import { HeadTemplate } from 'src/components/templates/HeadTemplate';
 
 import { GLOBAL_STYLE } from 'src/styles/global_style';
 
+const createApolloClient = new ApolloClient({
+  uri: 'http://localhost:8080/v1/graphql',
+  cache: new InMemoryCache(),
+});
+
 const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <React.Fragment>
       <HeadTemplate />
-      <Global
-        styles={css`
-          ${GLOBAL_STYLE}
-        `}
-      />
-      <Component {...pageProps} />
+      <ApolloProvider client={createApolloClient}>
+        <Global
+          styles={css`
+            ${GLOBAL_STYLE}
+          `}
+        />
+        <Component {...pageProps} />
+      </ApolloProvider>
     </React.Fragment>
   );
 };
