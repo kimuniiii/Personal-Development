a8ec9c9c1376ab528ad54ce12c9e2d2c4a062d4d
k-kimura <kk.baseball.0824@gmail.com>
2021-09-01 22:31:32 +0900

    サーバーサイドで行うログアウト処理関数の実装

---
+++ packages/client/src/pages/api/logout/index.tsx
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import auth0 from 'src/pages/api/lib/auth0';
+
+/**
+ * @概要 auth0のhandleLogoutメソッドを使ってログアウト処理を行う関数
+ * @説明 ログアウトボタン押下時には「/api/logout」にアクセスして、postLogoutRedirectUriでセットしたURLに遷移する
+ */
+const logout = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  try {
+    await auth0.handleLogout(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(error.status || 500).end(error.message);
+  }
+};
+
+export default logout;
