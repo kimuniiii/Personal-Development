75e7d619f62a9a8862f4e20d3f4a7650768532e3
k-kimura <kk.baseball.0824@gmail.com>
2021-09-01 22:28:01 +0900

    auth0のページで認証後、callbackAPIにリダイレクトされてクッキーを生成する関数の実装

---
+++ packages/client/src/pages/api/callback/index.tsx
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import auth0 from 'src/pages/api/lib/auth0';
+
+/**
+ * @概要 auth0のページで認証後、このAPIにリダイレクトされてクッキーを生成する関数
+ * @説明 クッキーを生成した後は、auth0で設定したREDIRECT_URIにリダイレクトされる
+ */
+const callback = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  try {
+    await auth0.handleCallback(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(error.status || 500).end(error.message);
+  }
+};
+
+export default callback;
