eb947cf24a6d36877d99836b71b077d66220bb09
k-kimura <kk.baseball.0824@gmail.com>
2021-09-03 04:29:09 +0900

    getRedirectUriOrigin 共通関数を作成

---
+++ packages/client/src/utils/getRedirectUriOrigin.ts
@@ -0,0 +1,13 @@
+import type { IncomingMessage } from 'http';
+
+/**
+ * @概要 ログイン後のリダイレクト先のオリジンを取得する関数
+ * @説明 開発環境と本番環境で、ログイン後のリダイレクト先のオリジンを出し分けたいため
+ */
+export const getRedirectUriOrigin = (req?: IncomingMessage): string | undefined => {
+  if (process.env.NEXT_PUBLIC_RIOT_DEVELOPMENT === 'development') {
+    return req?.headers?.origin;
+  } else {
+    return 'http://localhost:3001';
+  }
+};
