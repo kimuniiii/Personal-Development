1b064ecdb6365dc09a06efe9a164b9c748fbd7a4
k-kimura <kk.baseball.0824@gmail.com>
2021-09-03 04:45:58 +0900

    これで本番環境でもログインできるはず！

--- packages/client/src/utils/getRedirectUriOrigin.ts
+++ packages/client/src/utils/getRedirectUriOrigin.ts
@@ -5,9 +5,9 @@ import type { IncomingMessage } from 'http';
  * @説明 開発環境と本番環境で、ログイン後のリダイレクト先のオリジンを出し分けたいため
  */
 export const getRedirectUriOrigin = (req?: IncomingMessage): string | undefined => {
-  if (process.env.RIOT_DEVELOPMENT !== 'development') {
-    return req?.headers?.origin;
-  } else {
+  if (process.env.NEXT_PUBLIC_RIOT_DEVELOPMENT === 'development') {
     return 'http://localhost:3001';
+  } else {
+    return req?.headers?.origin;
   }
 };
