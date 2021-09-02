84037d7a32b3ad5697f7c181ef5bcc22905b0ed5
k-kimura <kk.baseball.0824@gmail.com>
2021-09-03 04:32:27 +0900

    開発環境と本番環境の条件分岐を間違えていたので修正

--- packages/client/src/utils/getRedirectUriOrigin.ts
+++ packages/client/src/utils/getRedirectUriOrigin.ts
@@ -5,7 +5,7 @@ import type { IncomingMessage } from 'http';
  * @説明 開発環境と本番環境で、ログイン後のリダイレクト先のオリジンを出し分けたいため
  */
 export const getRedirectUriOrigin = (req?: IncomingMessage): string | undefined => {
-  if (process.env.NEXT_PUBLIC_RIOT_DEVELOPMENT === 'development') {
+  if (process.env.NEXT_PUBLIC_RIOT_PRODUCTION === 'production') {
     return req?.headers?.origin;
   } else {
     return 'http://localhost:3001';
