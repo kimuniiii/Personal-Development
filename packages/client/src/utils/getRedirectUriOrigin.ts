d8d12c7f37d93601a9149474caacf00c13dbe7db
k-kimura <kk.baseball.0824@gmail.com>
2021-09-07 02:30:13 +0900

    ♻️ getRedirectUriOrigin を環境ごとに条件分岐

--- packages/client/src/utils/getRedirectUriOrigin.ts
+++ packages/client/src/utils/getRedirectUriOrigin.ts
@@ -3,9 +3,11 @@
  * @説明 開発環境と本番環境で、ログイン後のリダイレクト先のオリジンを出し分けたいため
  */
 export const getRedirectUriOrigin = (): string => {
-  if (process.env.NEXT_PUBLIC_RIOT_DEVELOPMENT === 'development') {
+  if (process.env.VERCEL_ENV === 'development') {
     return 'http://localhost:3001';
+  } else if (process.env.VERCEL_ENV === 'staging') {
+    return 'https://personal-development-client-git-develop-kimuniiii.vercel.app';
   } else {
-    return 'https://riot-ec-site.com/';
+    return 'https://riot-ec-site.com';
   }
 };
