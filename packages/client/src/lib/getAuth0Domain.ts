b37ae4c53d1f759a6b6eb83d253d089d77297c47
k-kimura <kk.baseball.0824@gmail.com>
2021-09-07 03:09:39 +0900

    ✨ getAuth0Domain関数の新規作成

---
+++ packages/client/src/lib/getAuth0Domain.ts
@@ -0,0 +1,18 @@
+/**
+ * @概要 各環境ごとに異なる「Auth0」の「clientId」を取得する関数
+ */
+export const getAuth0Domain = (): string => {
+  if (process.env.VERCEL_ENV === 'development') {
+    return process.env.NEXT_PUBLIC_AUTH0_DOMAIN || '';
+  }
+
+  if (process.env.VERCEL_ENV === 'staging') {
+    return process.env.VERCEL_AUTH0_DOMAIN || '';
+  }
+
+  if (process.env.VERCEL_ENV === 'production') {
+    return process.env.VERCEL_AUTH0_DOMAIN || '';
+  }
+
+  return '';
+};
