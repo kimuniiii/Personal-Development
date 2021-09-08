980e0615606680f20c2c84d7a4604136de13a50f
kimuniiii <kimuniiii@users.noreply.github.com>
2021-09-08 09:33:04 +0000

    Deploying to gh-pages from @ kimuniiii/Personal-Development@f3fd030a3b863b329439d6db800b9f9fdc45d723 🚀

--- font_size.ts
+++
@@ -1,9 +0,0 @@
-export const FONT_SIZE = {
-  FS_12: '12px',
-  FS_14: '14px',
-  FS_16: '16px',
-  FS_18: '18px',
-  FS_20: '20px',
-  FS_24: '24px',
-  FS_32: '32px',
-} as const;
--- font_weight.ts
+++
@@ -1,4 +0,0 @@
-export const FONT_WEIGHT = {
-  BOLD: 600,
-  NORMAL: 300,
-} as const;
