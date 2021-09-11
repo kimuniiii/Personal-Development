0753dacce9f3aa6f0ad708ee8531c5bc55d022a7
k-kimura <kk.baseball.0824@gmail.com>
2021-09-11 17:22:41 +0900

    📝 絶対パスを返す関数にコメントを追加

--- packages/client/src/utils/getSrcAbsolutePath.ts
+++ packages/client/src/utils/getSrcAbsolutePath.ts
@@ -1,6 +1,10 @@
+/**
+ * @概要 画像のパスを絶対パスにして返す関数
+ * @説明 OGP画像のURLを生成するときに使用する
+ */
 export const getSrcAbsolutePath = (imgSrc: string): string => {
   const protocol = process.env.VERCEL_ENV === 'production' ? 'https:' : 'http:';
-  const host = process.env.VERCEL_ENV === 'production' ? 'riot-ec-site.com' : 'localhost';
+  const host = process.env.VERCEL_ENV === 'production' ? 'www.riot-ec-site.com' : 'localhost';
 
   return `${protocol}//${host}${imgSrc}`;
 };
