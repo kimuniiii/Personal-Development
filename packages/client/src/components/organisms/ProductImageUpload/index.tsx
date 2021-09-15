d0baec24323b9be96dab753de73c3daf63725f78
k-kimura <kk.baseball.0824@gmail.com>
2021-09-16 01:07:32 +0900

    🎨 商品登録用の画像アップロードコンポーネント実装完了

--- packages/client/src/components/organisms/ProductImageUpload/index.tsx
+++ packages/client/src/components/organisms/ProductImageUpload/index.tsx
@@ -9,74 +9,91 @@ import { Margin } from 'src/components/layouts/Margin';
 import { COLOR_PALETTE } from 'src/styles/color_palette';
 import { FONT_SIZE } from 'src/styles/font_size';
 
-import ReactImage from '../../../../public/images/react.jpg';
+type ProductImageUploadProps = {
+  photoFiles: File[];
+  onDeleteImgBtn: (photoIndex: number) => void;
+  onFileInputChange: React.ChangeEventHandler<HTMLInputElement>;
+};
 
 /**
  * @概要 商品登録ページで商品画像を最大3枚までアップロードできるコンポーネント
  */
-export const ProductImageUpload: VFC = () => {
+export const ProductImageUpload: VFC<ProductImageUploadProps> = ({
+  photoFiles,
+  onDeleteImgBtn,
+  onFileInputChange,
+}) => {
   return (
     <React.Fragment>
       <StImageContainer>
-        {[...Array(3)].map((_: number, idx: number) => {
-          return (
-            <StImagePosition key={idx}>
-              <IconButton
-                className='icon-close-button'
-                type='button'
-                svgComponent={<ImCross size={16} />}
-                width='40px'
-                height='40px'
-                borderRadius='50%'
-                onClick={(): void => alert('IconButton Clicked')}
-              />
-              <Image src={ReactImage} alt='Reactの画像です' />
-            </StImagePosition>
-          );
-        })}
+        {[...Array(3)].map((_: number, idx: number) =>
+          idx < photoFiles.length ? (
+            <React.Fragment key={idx}>
+              <StImagePosition>
+                <IconButton
+                  className='icon-close-button'
+                  type='button'
+                  svgComponent={<ImCross size={16} />}
+                  width='40px'
+                  height='40px'
+                  borderRadius='50%'
+                  onClick={(): void => onDeleteImgBtn(idx)}
+                />
+                <Image
+                  src={URL.createObjectURL(photoFiles[idx])}
+                  alt='Reactの画像です'
+                  layout='fill'
+                />
+              </StImagePosition>
+              <Margin right='16px' />
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={idx}>
+              <StImagePlaceholderBox />
+              <Margin right='16px' />
+            </React.Fragment>
+          ),
+        )}
       </StImageContainer>
       <Margin bottom='16px' />
-      <StFileUploadBtnContainer>
-        <StLabel>
-          写真を追加
-          <input type='file' />
-        </StLabel>
-      </StFileUploadBtnContainer>
+      <StLabel>
+        商品の写真を追加する（最大3枚まで）
+        <input type='file' onChange={onFileInputChange} />
+      </StLabel>
     </React.Fragment>
   );
 };
 
 const StImageContainer = styled.section`
   display: flex;
-  gap: 16px;
-  max-width: 500px;
+`;
+
+const StImagePlaceholderBox = styled.div`
+  width: 104px;
+  height: 100px;
+  background-color: ${COLOR_PALETTE.LIGHT_GRAY};
 `;
 
 const StImagePosition = styled.div`
   position: relative;
-  width: 157px;
-  height: 157px;
+  width: 104px;
+  height: 100px;
 
   .icon-close-button {
     position: absolute;
     top: -16px;
-    left: 128px;
+    left: 80px;
     /* 画像よりもボタンを上に表示させるため */
     z-index: 1;
   }
-`;
-
-const StFileUploadBtnContainer = styled.section`
-  display: flex;
-  flex-direction: row-reverse;
-  max-width: 500px;
 `;
 
 const StLabel = styled.label`
   display: inline-block;
   text-align: center;
   padding: 12px 16px;
-  width: 157px;
+  max-width: 343px;
+  width: 343px;
   border-radius: 3px;
   background-color: #3e8bff;
   color: ${COLOR_PALETTE.WHITE};
