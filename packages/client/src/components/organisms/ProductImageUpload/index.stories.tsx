6086de27f9b63a3b630c9f7708608111113e702b
Kimura Kazuki <kk.baseball.0824@gmail.com>
2021-09-17 10:05:49 +0900

    🎨 onChangeは連続で同じファイルを選択すると発火しないの対応

--- packages/client/src/components/organisms/ProductImageUpload/index.stories.tsx
+++ packages/client/src/components/organisms/ProductImageUpload/index.stories.tsx
@@ -27,6 +27,8 @@ const Template: Story<Props> = () => {
     }
     setPhotoFiles([...photoFiles, ...event.target.files]);
     // onChangeは連続で同じファイルを選択すると発火しない問題の対応のため
+    // 初期化することで同じファイルを連続で選択してもonChangeが発動するように設定する
+    // こうすることで、画像をキャンセルしてすぐに同じ画像を選ぶ動作に対応できる
     event.target.value = '';
   };
 
