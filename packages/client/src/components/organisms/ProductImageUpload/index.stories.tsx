aad1feff5707690d4219d2ea0df04cdf4b9ab064
Kimura Kazuki <kk.baseball.0824@gmail.com>
2021-09-17 13:52:59 +0900

    🎨 要件整理のためのコメントアウト追加

--- packages/client/src/components/organisms/ProductImageUpload/index.stories.tsx
+++ packages/client/src/components/organisms/ProductImageUpload/index.stories.tsx
@@ -24,10 +24,13 @@ const Template: Story<Props> = () => {
   };
 
   const onFileInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    // 型ガード（Nullチェック）
     if (event.target.files === null || event.target.files.length === 0) {
       return;
     }
 
+    // 画像のみアップロードするようにするため
+    // 画像以外のファイルだったらエラー文を表示する。処理を中断する。
     if (
       !['image/gif', 'image/jpeg', 'image/png', 'image/bmp', 'image/svg+xml'].includes(
         event.target.files[0].type,
@@ -37,6 +40,8 @@ const Template: Story<Props> = () => {
       return;
     }
 
+    // 商品登録ページのアップロードできる画像（＝プレビューの画像）の枚数は3枚までにするため
+    // 3枚以上のファイルをアップロードしようとしたらエラー文を出す。処理を中断する。
     if (photoFiles.length >= 3) {
       setIsNumberError(true);
       return;
