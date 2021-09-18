67cf0dfd42072c39066b2dadd197c34ad7b12ff6
k-kimura <kk.baseball.0824@gmail.com>
2021-09-19 00:56:49 +0900

    🎨 商品登録ページの画像送信までの実装完了

--- packages/client/src/components/organisms/ProductImageUpload/index.stories.tsx
+++ packages/client/src/components/organisms/ProductImageUpload/index.stories.tsx
@@ -1,7 +1,8 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
 import React, { useState } from 'react';
 
-import { validations } from 'src/utils/validate';
+import { Button } from 'src/components/atoms/Button';
+import { Margin } from 'src/components/layouts/Margin';
 
 import { ProductImageUpload } from '.';
 
@@ -13,108 +14,38 @@ export default {
 type Props = React.ComponentProps<typeof ProductImageUpload>;
 
 const Template: Story<Props> = (args) => {
-  const [photoFiles, setPhotoFiles] = useState<File[]>([]);
-  const [isFileTypeError, setIsFileTypeError] = useState(false);
-  const [isNumberError, setIsNumberError] = useState(false);
-  const [isSameImgSizeError, setIsSameImgSizeError] = useState(false);
-  const [isMaxImgSizeError, setIsMaxImgSizeError] = useState(false);
+  // ProductImageUpload に関する状態管理と更新関数とイベントハンドラ
+  const [selectedFiles, setPhotoFiles] = useState<File[]>([]);
 
   /**
-   * @概要 全てのエラーを一度リセットするため関数
+   * @概要 送信ボタンを押した時に呼び出されるイベントハンドラ
    */
-  const resetErrors = (): void => {
-    setIsFileTypeError(false);
-    setIsSameImgSizeError(false);
-    setIsNumberError(false);
-    setIsMaxImgSizeError(false);
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    console.log('selectedFiles', selectedFiles);
+    console.log({ profileImage: selectedFiles });
   };
 
-  const onDeleteImgBtn = (photoIndex: number): void => {
-    if (confirm('選択した画像を消してよろしいですか？')) {
-      resetErrors();
-      const modifyPhotos = photoFiles.concat();
-      modifyPhotos.splice(photoIndex, 1);
-      setPhotoFiles(modifyPhotos);
-    }
+  /**
+   * @概要 子供から親に送られたファイル情報を更新する関数
+   */
+  const onFileSelect = (selectedFiles: File[]): void => {
+    setPhotoFiles(selectedFiles);
   };
-
-  const onFileInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    // 型ガード（Nullチェック）
-    if (event.target.files === null || event.target.files.length === 0) {
-      return;
-    }
-
-    resetErrors();
-
-    // 10MB以上の画像はアップロードしないように弾くため
-    if (event.target.files[0].size >= validations.maxImageSize) {
-      setIsMaxImgSizeError(true);
-      return;
-    }
-
-    // 同じ画像はアップロードしないように弾くため
-    // 同じサイズの画像は配列に追加できないというロジックで実装
-    // 同じサイズの画像だったらエラー文を表示する。処理を中断する。
-    const existsSameSizeImg = photoFiles.some((photo) => {
-      // 型ガード（Nullチェック）
-      if (event.target.files === null || event.target.files.length === 0) {
-        return false;
-      }
-
-      return photo.size === event.target.files[0].size;
-    });
-
-    if (existsSameSizeImg) {
-      setIsSameImgSizeError(true);
-      return;
-    }
-
-    // 画像のみアップロードするようにするため
-    // 画像以外のファイルだったらエラー文を表示する。処理を中断する。
-    if (
-      !['image/gif', 'image/jpeg', 'image/png', 'image/bmp', 'image/svg+xml'].includes(
-        event.target.files[0].type,
-      )
-    ) {
-      setIsFileTypeError(true);
-      return;
-    }
-
-    // 商品登録ページのアップロードできる画像（＝プレビューの画像）の枚数は3枚までにするため
-    // 3枚以上のファイルをアップロードしようとしたらエラー文を出す。処理を中断する。
-    if (photoFiles.length >= 3) {
-      setIsNumberError(true);
-      return;
-    }
-
-    console.log('event.target.value', event.target.value);
-    console.log('event.target.files', event.target.files);
-    console.log('event.target.files[0]', event.target.files[0]);
-    console.log('forward photoFiles', photoFiles);
-
-    setPhotoFiles([...photoFiles, ...event.target.files]);
-
-    console.log('after photoFiles', photoFiles);
-
-    // onChangeは連続で同じファイルを選択すると発火しない問題の対応のため
-    // 初期化することで同じファイルを連続で選択してもonChangeが発動するように設定する
-    // こうすることで、画像をキャンセルしてすぐに同じ画像を選ぶ動作に対応できる
-    event.target.value = '';
-  };
-
-  console.log('out event handler photoFiles', photoFiles);
 
   return (
-    <ProductImageUpload
-      {...args}
-      photoFiles={photoFiles}
-      isFileTypeError={isFileTypeError}
-      isNumberError={isNumberError}
-      isSameImgSizeError={isSameImgSizeError}
-      isMaxImgSizeError={isMaxImgSizeError}
-      onDeleteImgBtn={onDeleteImgBtn}
-      onFileInputChange={onFileInputChange}
-    />
+    <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit}>
+      <ProductImageUpload {...args} selectedFiles={selectedFiles} onFileSelect={onFileSelect} />
+      <Margin bottom='16px' />
+      <Button
+        type='submit'
+        buttonContent='送信ボタン'
+        width='343px'
+        styleTypes='primary'
+        fontSizeValue='16px'
+        onClick={(): void => alert('送信ボタンをクリック')}
+      />
+    </form>
   );
 };
 
