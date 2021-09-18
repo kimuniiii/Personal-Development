ec3257a5bc880092ff38f11fd35459eb009182c3
k-kimura <kk.baseball.0824@gmail.com>
2021-09-19 00:20:54 +0900

    🎨 Storybookで動作確認できるようにコードを変更

--- packages/client/src/components/organisms/ProfileImageUpload/index.stories.tsx
+++ packages/client/src/components/organisms/ProfileImageUpload/index.stories.tsx
@@ -1,6 +1,9 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
 import React, { useState } from 'react';
 
+import { Button } from 'src/components/atoms/Button';
+import { Margin } from 'src/components/layouts/Margin';
+
 import { ProfileImageUpload } from '.';
 
 export default {
@@ -11,64 +14,44 @@ export default {
 type Props = React.ComponentProps<typeof ProfileImageUpload>;
 
 const Template: Story<Props> = (args) => {
+  // プロフィール編集画像に関する「状態変数」と「更新関数」と「イベントハンドラ」
+  const [selectedFile, setSelectedFile] = useState<File>();
   const [imageUrl, setImageUrl] = useState<string>('');
-  const [imageFileSize, setImageFileSize] = useState(0);
-  const [isFileTypeError, setIsFileTypeError] = useState(false);
 
-  /**
-   * @概要 全てのエラーを一度リセットするため関数
-   */
-  const resetErrors = (): void => {
-    setIsFileTypeError(false);
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    console.log('selectedFile', selectedFile);
   };
 
-  const onFileInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    if (event.target.files === null || event.target.files.length === 0) {
-      return;
-    }
-
-    resetErrors();
-
-    if (
-      !['image/gif', 'image/jpeg', 'image/png', 'image/bmp', 'image/svg+xml'].includes(
-        event.target.files[0].type,
-      )
-    ) {
-      setIsFileTypeError(true);
-      return;
-    }
-
-    console.log('event.target.value', event.target.value);
-    console.log('event.target.files', event.target.files);
-    console.log('event.target.files[0]', event.target.files[0]);
-
-    const imageFile = event.target.files[0];
-    const imageUrl = URL.createObjectURL(imageFile);
-    setImageUrl(imageUrl);
-    setImageFileSize(event.target.files[0].size);
-
-    // onChangeは連続で同じファイルを選択すると発火しない問題の対応のため
-    // 初期化することで同じファイルを連続で選択してもonChangeが発動するように設定する
-    // こうすることで、画像をキャンセルしてすぐに同じ画像を選ぶ動作に対応できる
-    event.target.value = '';
+  const onFileSelect = (selectedFile: File): void => {
+    setSelectedFile(selectedFile);
+    setImageUrl(URL.createObjectURL(selectedFile));
   };
 
   const deleteProfileImg = (): void => {
     if (confirm('選択した画像を削除してもよろしいですか？')) {
-      resetErrors();
       setImageUrl('');
     }
   };
 
   return (
-    <ProfileImageUpload
-      {...args}
-      imageUrl={imageUrl}
-      imageFileSize={imageFileSize}
-      isFileTypeError={isFileTypeError}
-      onClick={deleteProfileImg}
-      onChange={onFileInputChange}
-    />
+    <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit}>
+      <ProfileImageUpload
+        {...args}
+        imageUrl={imageUrl}
+        onClick={deleteProfileImg}
+        onFileSelect={onFileSelect}
+      />
+      <Margin bottom='16px' />
+      <Button
+        type='submit'
+        buttonContent='送信ボタン'
+        width='343px'
+        styleTypes='primary'
+        fontSizeValue='16px'
+        onClick={(): void => alert('送信ボタンをクリック')}
+      />
+    </form>
   );
 };
 
@@ -76,4 +59,5 @@ export const Basic = Template.bind({});
 
 Basic.args = {
   labelText: 'プロフィール画像',
+  name: 'profileImage',
 };
