fb24be38391a4f473d4dc0893ce70ca5771e8b7d
k-kimura <kk.baseball.0824@gmail.com>
2021-09-26 15:45:55 +0900

    🎨 create getApiEndPoint Function

---
+++ packages/client/src/utils/getApiEndPoint/index.ts
@@ -0,0 +1,22 @@
+import {
+  DEVELOPMENT_GRAPHQL_API_ENDPOINT,
+  STAGING_GRAPHQL_API_ENDPOINT,
+  PRODUCTION_GRAPHQL_API_ENDPOINT,
+} from 'src/constants';
+
+/**
+ * @概要 ログイン後のリダイレクト先のオリジンを取得する関数
+ * @説明 開発環境と本番環境で、ログイン後のリダイレクト先のオリジンを出し分けたいため
+ */
+export const getApiEndPoint = (vercelEnv?: string): string => {
+  switch (vercelEnv) {
+    case 'development':
+      return DEVELOPMENT_GRAPHQL_API_ENDPOINT;
+    case 'staging':
+      return STAGING_GRAPHQL_API_ENDPOINT;
+    case 'production':
+      return PRODUCTION_GRAPHQL_API_ENDPOINT;
+    default:
+      return '';
+  }
+};
