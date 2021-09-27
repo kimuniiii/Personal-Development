ebbc46610d2522db9ae59da939844018b7a2aa45
k-kimura <kk.baseball.0824@gmail.com>
2021-09-28 08:00:39 +0900

    🎨 create X_HASURA_ADMIN_SECRET in constans

--- packages/client/src/constants/index.ts
+++ packages/client/src/constants/index.ts
@@ -1,5 +1,9 @@
 export const COPY_RIGHT = `Copyright(c) ${new Date().getFullYear()} RIOT inc. All Rights Reserved`;
 
+// 開発環境・ステージング環境・本番環境の「HASURA_GRAPHQL_ADMIN_SECRET」
+// GraphQL EndPoint を「管理者シークレット」で保護するため
+export const X_HASURA_ADMIN_SECRET = 'heroku-admin-secret';
+
 // 開発環境・ステージング環境・本番環境の「オリジン」
 export const VERCEL_DEVELOPMENT_ORIGIN = 'http://localhost:3020';
 export const VERCEL_STAGING_ORIGIN =
