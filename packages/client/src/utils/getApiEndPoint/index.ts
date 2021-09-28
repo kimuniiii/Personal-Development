799c0c443d7c447cb06a66bc3f5a7af35bbb209f
k-kimura <kk.baseball.0824@gmail.com>
2021-09-28 11:46:03 +0900

    🎨 delete develop_api_endpoint in getApiEndPont

--- packages/client/src/utils/getApiEndPoint/index.ts
+++ packages/client/src/utils/getApiEndPoint/index.ts
@@ -1,8 +1,4 @@
-import {
-  DEVELOPMENT_GRAPHQL_API_ENDPOINT,
-  STAGING_GRAPHQL_API_ENDPOINT,
-  PRODUCTION_GRAPHQL_API_ENDPOINT,
-} from 'src/constants';
+import { STAGING_GRAPHQL_API_ENDPOINT, PRODUCTION_GRAPHQL_API_ENDPOINT } from 'src/constants';
 
 /**
  * @概要 ログイン後のリダイレクト先のオリジンを取得する関数
@@ -11,7 +7,6 @@ import {
 export const getApiEndPoint = (vercelEnv?: string): string => {
   switch (vercelEnv) {
     case 'development':
-      return DEVELOPMENT_GRAPHQL_API_ENDPOINT;
     case 'staging':
       return STAGING_GRAPHQL_API_ENDPOINT;
     case 'production':
