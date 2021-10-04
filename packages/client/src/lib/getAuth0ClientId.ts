2591d6c0542218adb5d2fefb67e0e03d7a4ac518
k-kimura <kk.baseball.0824@gmail.com>
2021-10-05 00:53:11 +0900

    🎨 環境変数を1つの環境変数に置換する

--- packages/client/src/lib/getAuth0ClientId.ts
+++ packages/client/src/lib/getAuth0ClientId.ts
@@ -5,8 +5,8 @@ export const getAuth0ClientId = (): string => {
   if (process.env.VERCEL_ENV === 'development') {
     return process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || '';
   } else if (process.env.VERCEL_ENV === 'staging') {
-    return process.env.VERCEL_STAGING_AUTH0_CLIENT_ID || '';
+    return process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || '';
   } else {
-    return process.env.VERCEL_PRODUCTION_AUTH0_CLIENT_ID || '';
+    return process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || '';
   }
 };
