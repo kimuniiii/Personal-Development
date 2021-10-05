14a6cf3819849caa1c37a3a977be493a06dee103
k-kimura <kk.baseball.0824@gmail.com>
2021-10-05 12:24:07 +0900

    🎨 Sp用の「ProductCard Component」を新規作成

---
+++ packages/client/src/components/organisms/ProductCard/Sp/index.tsx
@@ -0,0 +1,102 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import styled from '@emotion/styled';
+import Image from 'next/image';
+import Router from 'next/router';
+import React from 'react';
+
+import type { VFC } from 'react';
+
+import { Margin } from 'src/components/layouts/Margin';
+
+import { COLOR_PALETTE } from 'src/styles/color_palette';
+
+import { priceToJapaneseYen } from 'src/utils/price';
+
+import NoImage from '../../../../../public/images/no_image.png';
+
+type Props = {
+  productCardList?: { id: number; name: string; price: number; base64_image: string }[];
+};
+
+export const SpProductCard: VFC<Props> = ({ productCardList }) => {
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+
+  /**
+   * @概要 商品画像をクリックしたときに呼ばれるイベントハンドラ
+   * @要件1 ログインしていたら、商品詳細画面に遷移できること
+   * @要件2 ログインしてないなら、Auth0のユーザー登録モーダルに遷移すること
+   */
+  const handleProductImageBtnClick = (): void => {
+    if (isAuthenticated) {
+      Router.push('/product/detail');
+    } else {
+      loginWithRedirect({
+        screen_hint: 'signup',
+      });
+    }
+  };
+
+  return (
+    <StProductList>
+      {productCardList?.map((productItem, idx) => {
+        return idx <= 5 ? (
+          <React.Fragment key={productItem.id}>
+            <StProductItem>
+              <StFigure>
+                <StImageBtn onClick={handleProductImageBtnClick}>
+                  {productItem.base64_image ? (
+                    <Image
+                      src={productItem.base64_image}
+                      alt='Product Image'
+                      width={348}
+                      height={180}
+                      objectFit='contain'
+                    />
+                  ) : (
+                    <Image src={NoImage} alt='No Image' width={126} height={126} />
+                  )}
+                </StImageBtn>
+                <figcaption>{productItem.name}</figcaption>
+                <figcaption>{priceToJapaneseYen(productItem.price)}</figcaption>
+              </StFigure>
+            </StProductItem>
+            {idx !== 2 && idx !== 5 ? <Margin right='16px' /> : null}
+          </React.Fragment>
+        ) : null;
+      })}
+    </StProductList>
+  );
+};
+
+const StProductList = styled.ul`
+  display: flex;
+  flex-flow: column wrap;
+  /* TODO : Safari で表示崩れが起きるため */
+  row-gap: 16px;
+  width: 348px;
+  padding-top: 16px;
+`;
+
+const StProductItem = styled.li`
+  width: 100%;
+  height: auto;
+  border-radius: 3px;
+  border: 1px solid ${COLOR_PALETTE.BLACK};
+`;
+
+const StFigure = styled.figure`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StImageBtn = styled.button`
+  cursor: pointer;
+  /* ホバーをなめらかにするため */
+  transition: opacity 0.3s;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`;
