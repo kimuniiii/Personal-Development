5b18a47c585b79978a76ad9bcc8f1ecf153cec26
k-kimura <kk.baseball.0824@gmail.com>
2021-10-08 12:18:41 +0900

    🎨  create TransitionBaseModal in organisms dir

---
+++ packages/client/src/components/organisms/TransitionBaseModal/index.tsx
@@ -0,0 +1,50 @@
+import styled from '@emotion/styled';
+
+import type { VFC } from 'react';
+import type { ValueOf } from 'typings/utils/ValueOf';
+
+import { FONT_SIZE } from 'src/styles/font_size';
+
+type TransitionBaseModalProps = {
+  mainContent: string | JSX.Element;
+  closeButtonComponent: JSX.Element;
+  titleContent?: string;
+  headerPadding?: string;
+  headerFontSize?: ValueOf<typeof FONT_SIZE>;
+};
+
+export const TransitionBaseModal: VFC<TransitionBaseModalProps> = ({
+  mainContent,
+  closeButtonComponent,
+  titleContent,
+  headerPadding,
+  headerFontSize,
+}) => {
+  return (
+    <StRootContainer>
+      <StModalTitleWrapper headerPadding={headerPadding}>
+        <StModalTitle headerFontSize={headerFontSize}>{titleContent}</StModalTitle>
+        {closeButtonComponent}
+      </StModalTitleWrapper>
+      <main>{mainContent}</main>
+    </StRootContainer>
+  );
+};
+
+const StRootContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StModalTitleWrapper = styled.section<Pick<TransitionBaseModalProps, 'headerPadding'>>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: ${({ headerPadding }): string | null =>
+    headerPadding !== undefined ? headerPadding : null};
+`;
+
+const StModalTitle = styled.h2<Pick<TransitionBaseModalProps, 'headerFontSize'>>`
+  font-size: ${({ headerFontSize }): ValueOf<typeof FONT_SIZE> =>
+    headerFontSize ?? FONT_SIZE.FS_24};
+`;
