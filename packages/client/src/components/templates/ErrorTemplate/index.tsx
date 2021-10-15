dd7779542a35ba577938cdc61b813921000697fc
k-kimura <kk.baseball.0824@gmail.com>
2021-10-15 17:45:19 +0900

    🎨  Error | 404 | ErrorTemplate リファクタリング完了

--- packages/client/src/components/templates/ErrorTemplate/index.tsx
+++ packages/client/src/components/templates/ErrorTemplate/index.tsx
@@ -1,8 +1,11 @@
 import styled from '@emotion/styled';
+import Link from 'next/link';
 
+import { OuterLink } from 'src/components/atoms/OuterLink';
 import { Margin } from 'src/components/layouts/Margin';
 
 import { COLOR_PALETTE } from 'src/styles/color_palette';
+import { FONT_SIZE } from 'src/styles/font_size';
 
 import type { VFC } from 'react';
 
@@ -22,6 +25,12 @@ export const ErrorTemplate: VFC<ErrorTemplateProps> = ({ error }) => {
       </h1>
       <Margin bottom='16px' />
       <p>{error.stack}</p>
+      <Margin bottom='16px' />
+      <Link href='/' passHref>
+        <OuterLink className='error-outer-link' href='/' fontSizeValue={FONT_SIZE.FS_24}>
+          Go back to home page
+        </OuterLink>
+      </Link>
     </StErrorContainer>
   );
 };
@@ -35,4 +44,13 @@ const StErrorContainer = styled.div`
   text-align: center;
   background-color: ${COLOR_PALETTE.WARNING_COLOR};
   color: ${COLOR_PALETTE.ERROR_COLOR};
+
+  .error-outer-link {
+    background-color: ${COLOR_PALETTE.WARNING_COLOR};
+    color: ${COLOR_PALETTE.ERROR_COLOR};
+
+    &:hover {
+      border-bottom: 1px solid ${COLOR_PALETTE.ERROR_COLOR};
+    }
+  }
 `;
