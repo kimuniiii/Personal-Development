799af852fa8cf776b45b0278042532e5637b8e07
k-kimura <kk.baseball.0824@gmail.com>
2021-10-16 09:50:11 +0900

    🎨  testには「期待値」しか書かないに則したテストコードを作成

--- packages/client/src/hooks/useToggle/index.test.ts
+++ packages/client/src/hooks/useToggle/index.test.ts
@@ -12,10 +12,12 @@ describe('useToggle Custom Hooks Test', () => {
       const { result } = renderHook(() => useToggle(false));
       expect(result.current.state).toBeFalsy();
     });
-    test('toggle関数が呼び出されたら`true`になること', () => {
-      const { result } = renderHook(() => useToggle(false));
-      act(() => result.current.toggle());
-      expect(result.current.state).toBeTruthy();
+    describe('toggle関数が呼び出された場合', () => {
+      test('state は `true`に変化すること', () => {
+        const { result } = renderHook(() => useToggle(false));
+        act(() => result.current.toggle());
+        expect(result.current.state).toBeTruthy();
+      });
     });
   });
   describe('initialState = true の場合', () => {
@@ -23,10 +25,12 @@ describe('useToggle Custom Hooks Test', () => {
       const { result } = renderHook(() => useToggle(true));
       expect(result.current.state).toBeTruthy();
     });
-    test('toggle関数が呼び出されたら`false`になること', () => {
-      const { result } = renderHook(() => useToggle(true));
-      act(() => result.current.toggle());
-      expect(result.current.state).toBeFalsy();
+    describe('toggle関数が呼び出された場合', () => {
+      test('state は `false`に変化すること', () => {
+        const { result } = renderHook(() => useToggle(true));
+        act(() => result.current.toggle());
+        expect(result.current.state).toBeFalsy();
+      });
     });
   });
 });
