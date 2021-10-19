5f38fbfc8075c3805cf4a5e1840a305441de26f8
k-kimura <kk.baseball.0824@gmail.com>
2021-10-19 20:05:24 +0900

    🎨  pages / api 経由 では 成功しなかった

--- packages/client/src/pages/api/delete/index.ts
+++ packages/client/src/pages/api/delete/index.ts
@@ -1,19 +1,19 @@
-import { NextApiResponse } from 'next';
-// import ManagementClient from 'auth0';
+import ManagementClient from 'auth0';
 
-import type { NextApiRequest } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-// const DEVELOPMENT_AUTH0_CLIENT_ID = 'lGXhyxHZ7KcY035Ng4zbvBYduiJBNBHY';
-// const DEVELOPMENT_AUTH0_CLIENT_SECRET =
-//   '4h_F-vUcsS9kgoUAnxw4JfMwKxg8S_xY50sVVhsEj2ZFeoOhKz5O0nkmcMmr0WzA';
+const DEVELOPMENT_AUTH0_CLIENT_ID = 'lGXhyxHZ7KcY035Ng4zbvBYduiJBNBHY';
+const DEVELOPMENT_AUTH0_CLIENT_SECRET =
+  '4h_F-vUcsS9kgoUAnxw4JfMwKxg8S_xY50sVVhsEj2ZFeoOhKz5O0nkmcMmr0WzA';
 
-// const management = new ManagementClient.ManagementClient({
-//   domain: 'dev-dt8p9ro2.us.auth0.com',
-//   clientId: DEVELOPMENT_AUTH0_CLIENT_ID,
-//   clientSecret: DEVELOPMENT_AUTH0_CLIENT_SECRET,
-//   scope: 'delete:users',
-// });
+const management = new ManagementClient.ManagementClient({
+  domain: 'dev-dt8p9ro2.us.auth0.com',
+  clientId: DEVELOPMENT_AUTH0_CLIENT_ID,
+  clientSecret: DEVELOPMENT_AUTH0_CLIENT_SECRET,
+  scope: 'delete:users',
+});
 
+// TODO : api / delete 経由だと「退会処理」は成功しなかった
 export default async function deleteAuth0User(
   req: NextApiRequest,
   res: NextApiResponse,
@@ -21,15 +21,20 @@ export default async function deleteAuth0User(
   try {
     const { auth0UserId } = req.query;
     console.log('auth0UserId', auth0UserId);
+    // ここに以下のコードがあればOK
     res.status(200).json({ auth0UserId });
-    // await management.deleteUser({ id: auth0UserId[0] }, function (err) {
-    //   if (err) {
-    //     // Handle error.
-    //     console.log('> Delete Auth0 user, err = ', err);
-    //   }
-    //   // User deleted.
-    //   console.log('Auth0 user deleted');
-    // });
+    const deleteUserResult = await management.deleteUser({ id: auth0UserId[0] }, function (err) {
+      if (err) {
+        // Handle error.
+        console.log('> Delete Auth0 user, err = ', err);
+        return;
+      }
+      // User deleted.
+      console.log('Auth0 user deleted');
+      res.status(200).json({ auth0UserId });
+    });
+    console.log(deleteUserResult);
+    return deleteUserResult;
   } catch (error) {
     console.error(error);
   }
